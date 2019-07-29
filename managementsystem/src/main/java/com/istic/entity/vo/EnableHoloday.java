79e5b5c88ca4fa5ed25c7ece62c394114eb63fc5
hch <abcd-1234>
2019-07-29 15:47:15 +0800

    1

--- managementsystem/src/main/java/com/istic/entity/vo/EnableHoloday.java
+++ managementsystem/src/main/java/com/istic/entity/vo/EnableHoloday.java
@@ -1,14 +1,22 @@
 package com.istic.entity.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 
 /**
  * Created by hch on 2019/7/29.
  */
 @Data
+@ApiModel(value = "启用禁用假期类型")
 public class EnableHoloday {
+    @NotNull(message = "id列表不能为空")
+    @ApiModelProperty(value = "id列表")
     private List<String> idList;
+    @NotNull(message = "status不能为空")
+    @ApiModelProperty(value = "启用1 禁用0")
     private Byte status;
 }
