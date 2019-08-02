d5ea637b5231f6b23f42bdb5841392fe47400691
hch <abcd-1234>
2019-08-02 09:32:58 +0800

    1

---
+++ managementsystem/src/main/java/com/istic/dao/LeaveOrderStatusMapper.java
@@ -0,0 +1,10 @@
+package com.istic.dao;
+
+import com.istic.entity.LeaveOrderStatus;
+import com.istic.util.TkMapper;
+
+/**
+* Created by Mybatis Generator on 2019/08/01
+*/
+public interface LeaveOrderStatusMapper extends TkMapper<LeaveOrderStatus> {
+}
